7e1d9727676aef62467bf51c0e149c7b5f28f400
Quaver127 <dolidzedavid2003@gmail.com>
2025-03-08 10:13:36 +0100

    Initialization

--- Assets/Scripts/Visuals/CustomCursor.cs
+++ Assets/Scripts/Visuals/CustomCursor.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class CustomCursor : MonoBehaviour
 {
-    void Start()
-    { // Now correctly refers to UnityEngine.Cursor
+    void Awake()
+    { 
+        Cursor.visible = false;
     }
 }
