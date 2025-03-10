5a019d4f6814b6d2560cce1250492f034754cc60
Quaver127 <dolidzedavid2003@gmail.com>
2025-03-10 16:37:35 +0100

    Upgrade

---
+++ Assets/BonusDamageItem.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusDamageItem : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public Attack attack;
+    public GameObject damageUpObject;
+    public GameObject bonusDesc;
+    [SerializeField] GameObject player;
+    private bool playerInTrigger = false;
+    public bool isUpgradePicked = false;
+
+
+    public void Start()
+    {
+        if (isUpgradePicked)
+        {
+            Destroy(damageUpObject);
+        }
+    }
+    public void Update()
+    {
+        if (playerInTrigger)
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                attack.dmgValue += 1f;
+                StartCoroutine(ShowDescription());
+                
+            }    
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerInTrigger = true;
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+        }
+    }
+
+    IEnumerator ShowDescription()
+    {
+        isUpgradePicked = true;
+        Debug.Log("FunctionIsBeingCalled");
+        controller.canMove = false;
+        bonusDesc.SetActive(true);
+        yield return new WaitForSeconds(5f);
+        controller.canMove = true;
+        bonusDesc.SetActive(false);
+        Destroy(damageUpObject);
+        
+    }
+}
