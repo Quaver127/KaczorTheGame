fda1725be943766e3226403773f4500d8fcc3ca8
Quaver127 <dolidzedavid2003@gmail.com>
2025-05-14 11:19:45 +0200

    heart change

--- Assets/Scripts/Player/HealthUI.cs
+++ Assets/Scripts/Player/HealthUI.cs
@@ -24,7 +24,6 @@ public class HealthUI : MonoBehaviour
         {
             Image newHeart = Instantiate(heartPrefab, transform);
             newHeart.sprite = fullHeartSprite;
-            newHeart.color = Color.red;
             hearts.Add(newHeart);
         }
     }
@@ -36,12 +35,10 @@ public class HealthUI : MonoBehaviour
             if (i < currentHealth)
             {
                 hearts[i].sprite = fullHeartSprite;
-                hearts[i].color = Color.red;
             }
             else
             {
                 hearts[i].sprite = emptyHeartSprite;
-                hearts[i].color = Color.white;
             }
         }
     }
