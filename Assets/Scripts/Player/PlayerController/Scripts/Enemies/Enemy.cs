d0f63a5d7d8d480f2219db3b387dbde6e96842df
Quaver127 <dolidzedavid2003@gmail.com>
2025-06-10 22:39:53 +0200

    rozstawiłem enemy i zmieniłem ich zachowanie

--- Assets/Scripts/Player/PlayerController/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Player/PlayerController/Scripts/Enemies/Enemy.cs
@@ -4,161 +4,137 @@ using System.Collections;
 using System.Collections.Generic;
 using Random = UnityEngine.Random;
 
+public class Enemy : MonoBehaviour
+{
+    public CharacterController2D controller;
+    public float life = 10;
 
-public class Enemy : MonoBehaviour {
+    private Rigidbody2D rb;
+    private bool facingRight = true;
 
-	public CharacterController2D controller;
-	public float life = 10;
-	private bool isPlat;
-	private bool isObstacle;
-	private Transform fallCheck;
-	private Transform wallCheck;
-	public LayerMask turnLayerMask;
-	private Rigidbody2D rb;
+    public float speed = 5f;
+    public bool isInvincible = false;
+    private bool isHitted = false;
 
+    [Header("Patrol Points")]
+    public Transform pointA;
+    public Transform pointB;
 
-	public float distance = 1f;
-	public LayerMask boxMask;
-	private GameObject box;
-	
+    private Transform currentTarget;
 
-	private bool facingRight = true;
-	
-	public float speed = 5f;
+    [Header("Loot")]
+    public List<LootItem> lootTable = new List<LootItem>();
 
-	public bool isInvincible = false;
-	private bool isHitted = false;
-	
-	//Loot Table
-	[Header("Loot")] public List<LootItem> lootTable = new List<LootItem>();
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        currentTarget = pointB; // Start moving toward point B
+    }
 
-	void Awake () {
-		fallCheck = transform.Find("FallCheck");
-		wallCheck = transform.Find("WallCheck");
-		rb = GetComponent<Rigidbody2D>();
-	}
+    void FixedUpdate()
+    {
+        if (life <= 0)
+        {
+            GetComponent<Animator>().SetBool("IsDead", true);
+            StartCoroutine(DestroyEnemy());
+            return;
+        }
 
-	private void Update()
-	{
-		RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.up * transform.localScale.y, distance, boxMask );
-		
-		if (hit.collider != null && hit.collider.gameObject.tag == "Pushable")
-		{
-			box = hit.collider.gameObject;
-			ApplyDamage(4);
-		}
-	}
-	
-	private void OnDrawGizmos()
-	{
-		Gizmos.color = Color.yellow;
-        
-		Gizmos.DrawLine(transform.position, (Vector2)transform.position + Vector2.down * transform.localScale.x * distance);
-	}
+        if (!isHitted && life > 0 && Mathf.Abs(rb.velocity.y) < 0.5f)
+        {
+            Patrol();
+        }
+    }
 
-	// Update is called once per frame
-	void FixedUpdate () {
+    void Patrol()
+    {
+        float direction = currentTarget.position.x - transform.position.x;
+        rb.velocity = new Vector2(Mathf.Sign(direction) * speed, rb.velocity.y);
 
-		if (life <= 0) {
-			transform.GetComponent<Animator>().SetBool("IsDead", true);
-			StartCoroutine(DestroyEnemy());
-		}
+        // Flip sprite if needed
+        if ((direction > 0 && !facingRight) || (direction < 0 && facingRight))
+        {
+            Flip();
+        }
 
-		if (!isHitted && life > 0 && Mathf.Abs(rb.velocity.y) < 0.5f)
-		{
-			if (isPlat && !isObstacle && !isHitted)
-			{
-				if (facingRight)
-				{
-					rb.velocity = new Vector2(-speed, rb.velocity.y);
-				}
-				else
-				{
-					rb.velocity = new Vector2(speed, rb.velocity.y);
-				}
-			}
-			else
-			{
-				Flip();
-			}
-		}
-	}
+        // Switch target if close
+        if (Mathf.Abs(direction) < 0.2f)
+        {
+            currentTarget = currentTarget == pointA ? pointB : pointA;
+        }
+    }
 
-	void Flip (){
-		// Switch the way the player is labelled as facing.
-		facingRight = !facingRight;
-		
-		// Multiply the player's x local scale by -1.
-		Vector3 theScale = transform.localScale;
-		theScale.x *= -1;
-		transform.localScale = theScale;
-	}
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
 
-	public void ApplyDamage(int damage) {
-		if (!isInvincible) 
-		{
-			float direction = damage / Mathf.Abs(damage);
-			damage = Mathf.Abs(damage);
-			transform.GetComponent<Animator>().SetBool("Hit", true);
-			life -= damage;
-			controller.Mana += controller.manaGain;
-			rb.velocity = Vector2.zero;
-			rb.AddForce(new Vector2(direction * 500f, 100f));
-			StartCoroutine(HitTime());
-		}
-	}
+    public void ApplyDamage(int damage)
+    {
+        if (!isInvincible)
+        {
+            float direction = damage / Mathf.Abs(damage);
+            damage = Mathf.Abs(damage);
+            GetComponent<Animator>().SetBool("Hit", true);
+            life -= damage;
+            controller.Mana += controller.manaGain;
+            rb.velocity = Vector2.zero;
+            rb.AddForce(new Vector2(direction * 500f, 100f));
+            StartCoroutine(HitTime());
+        }
+    }
 
-	void OnCollisionStay2D(Collision2D collision)
-	{
-		if (collision.gameObject.tag == "Player" && life > 0)
-		{
-			collision.gameObject.GetComponent<CharacterController2D>().ApplyDamage(1, transform.position);
-		}
-	}
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && life > 0)
+        {
+            collision.gameObject.GetComponent<CharacterController2D>().ApplyDamage(1, transform.position);
+        }
+    }
 
-	IEnumerator HitTime()
-	{
-		isHitted = true;
-		isInvincible = true;
-		yield return new WaitForSeconds(0.1f);
-		isHitted = false;
-		isInvincible = false;
-	}
+    IEnumerator HitTime()
+    {
+        isHitted = true;
+        isInvincible = true;
+        yield return new WaitForSeconds(0.1f);
+        isHitted = false;
+        isInvincible = false;
+    }
 
-	IEnumerator DestroyEnemy()
-	{
-		CapsuleCollider2D capsule = GetComponent<CapsuleCollider2D>();
-		capsule.size = new Vector2(1f, 0.25f);
-		capsule.offset = new Vector2(0f, -0.8f);
-		capsule.direction = CapsuleDirection2D.Horizontal;
-		yield return new WaitForSeconds(0.25f);
-		rb.velocity = new Vector2(0, rb.velocity.y);
-		yield return new WaitForSeconds(0.2f);
-		Die();
-		
-	}
+    IEnumerator DestroyEnemy()
+    {
+        CapsuleCollider2D capsule = GetComponent<CapsuleCollider2D>();
+        capsule.size = new Vector2(1f, 0.25f);
+        capsule.offset = new Vector2(0f, -0.8f);
+        capsule.direction = CapsuleDirection2D.Horizontal;
+        yield return new WaitForSeconds(0.25f);
+        rb.velocity = new Vector2(0, rb.velocity.y);
+        yield return new WaitForSeconds(0.2f);
+        Die();
+    }
 
-	public void Die()
-	{
-		foreach (LootItem lootItem in lootTable)
-		{
-			if (Random.Range(0f, 100f) <= lootItem.dropChance)
-			{
-				InstantiateLoot(lootItem.itemPrefab);
-				break;	
-			}
-			
-		}
-		Destroy(gameObject);
-	}
+    public void Die()
+    {
+        foreach (LootItem lootItem in lootTable)
+        {
+            if (Random.Range(0f, 100f) <= lootItem.dropChance)
+            {
+                InstantiateLoot(lootItem.itemPrefab);
+                break;
+            }
+        }
+        Destroy(gameObject);
+    }
 
-	void InstantiateLoot(GameObject loot)
-	{
-		if (loot)
-		{
-			GameObject droppedLoot = Instantiate(loot, transform.position, Quaternion.identity);
-			
-			droppedLoot.GetComponent<SpriteRenderer>().color = Color.red;
-		}
-	}
+    void InstantiateLoot(GameObject loot)
+    {
+        if (loot)
+        {
+            GameObject droppedLoot = Instantiate(loot, transform.position, Quaternion.identity);
+            droppedLoot.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    }
 }
